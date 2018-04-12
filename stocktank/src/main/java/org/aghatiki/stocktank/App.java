f3d4d0f8dad3300f060f4f2b34ae7605123271fa
Akhil Ghatiki <akhil.ghatiki@gmail.com>
2018-04-12 22:43:06 +0530

    First commit with empty project structure

---
+++ stocktank/src/main/java/org/aghatiki/stocktank/App.java
@@ -0,0 +1,13 @@
+package org.aghatiki.stocktank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
